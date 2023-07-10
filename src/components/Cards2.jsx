67ac9c4a0a3a8d6f4c068a8a9e72e1006c026f94
¨kervys¨ <kervysjulian@gmail.com>
2023-07-09 21:59:41 -0400

    Subiendo cambios de RAting

--- src/components/Cards2.jsx
+++ src/components/Cards2.jsx
@@ -1,3 +1,200 @@
+// import React, { useState } from "react";
+// import { Link } from "react-router-dom";
+// import { useDispatch, useSelector } from "react-redux";
+// import {
+//   Box,
+//   Flex,
+//   Image,
+//   Badge,
+//   Tooltip,
+//   VStack,
+//   Button,
+//   Collapse,
+//   Icon,
+//   Popover,
+//   PopoverTrigger,
+//   PopoverContent,
+//   PopoverHeader,
+//   PopoverBody,
+//   Input,
+//   chakra,
+// } from "@chakra-ui/react";
+// import { FiShoppingCart } from "react-icons/fi";
+// import { BsStarFill, BsStarHalf, BsStar } from "react-icons/bs";
+// import { addProductToCart } from "../redux/actions";
+// import Rating from "./Rating";
+
+// const ProductAddToCart = ({ id, image, name, price, rating, numReviews }) => {
+//   // Dispatch para agregar productos al carrito
+//   const dispatch = useDispatch();
+//   const cartItems = useSelector((state) => state.cartItems);
+
+//   // Estado local para el popover de calificación y comentario
+//   const [isOpen, setIsOpen] = useState(false);
+//   const [ratingValue, setRatingValue] = useState(0);
+//   const [comment, setComment] = useState("");
+
+//   // Manejador para agregar productos al carrito
+//   const addProductToCartHandler = (product) => {
+//     dispatch(addProductToCart(product));
+//   };
+
+//   // Obtener la cantidad del producto en el carrito
+//   const getProductQuantityInCart = () => {
+//     const item = cartItems.find((item) => item.id === id);
+//     return item ? item.quantity : 0;
+//   };
+
+//   // Determinar si mostrar el icono del carrito en el botón
+//   const cartIconVisible = getProductQuantityInCart() > 0;
+
+//   // Manejador para el clic en una estrella de calificación
+//   const handleRatingClick = (value) => {
+//     setRatingValue(value);
+//     setIsOpen(false); // Cerrar el popover después de hacer clic en una estrella
+//   };
+
+//   // Manejador para el cambio de comentario
+//   const handleCommentChange = (event) => {
+//     setComment(event.target.value);
+//   };
+
+//   return (
+//     <Box
+//       bg="white"
+//       w="250px"
+//       h="340px"
+//       borderWidth="3px"
+//       rounded="lg"
+//       shadow="lg"
+//       position="relative"
+//       margin="10px"
+//       gridAutoRows="1"
+//     >
+//       <Flex direction="column">
+//         <VStack>
+//           <Box h="220px">
+//             <Flex>
+//               <Link to={`/detail/${id}`}>
+//                 <Image
+//                   src={image}
+//                   alt={`Picture of ${name}`}
+//                   roundedTop="lg"
+//                   maxH="200px"
+//                 />
+//               </Link>
+//             </Flex>
+//           </Box>
+//           <Box>
+//             <Flex>
+//               <Box>
+//                 <Flex
+//                   mt="1"
+//                   justifyContent="space-between"
+//                   alignContent="center"
+//                 >
+//                   <Box
+//                     fontSize="13px"
+//                     fontWeight="semibold"
+//                     as="h4"
+//                     lineHeight="tight"
+//                     isTruncated
+//                     h="30px"
+//                     w="200px"
+//                   >
+//                     {name}
+//                   </Box>
+//                 </Flex>
+
+//                 <Flex>
+//                   <Button
+//                     colorScheme="facebook"
+//                     size="sm"
+//                     onClick={() =>
+//                       addProductToCartHandler({ id, image, name, price })
+//                     }
+//                   >
+//                     <Icon as={FiShoppingCart} />
+//                     Add to Cart
+//                   </Button>
+
+//                   <Collapse in={cartIconVisible}>
+//                     <Tooltip
+//                       label="Go to cart"
+//                       bg="white"
+//                       placement="top"
+//                       color="gray.800"
+//                       fontSize="1.2em"
+//                     >
+//                       <Flex>
+//                         <Link to="/cart">
+//                           <Icon
+//                             as={FiShoppingCart}
+//                             h={7}
+//                             w={7}
+//                             alignSelf="center"
+//                           />
+//                           <Badge
+//                             ml={1}
+//                             colorScheme="red"
+//                             position="absolute"
+//                             borderRadius="full"
+//                           >
+//                             {getProductQuantityInCart()}
+//                           </Badge>
+//                         </Link>
+//                       </Flex>
+//                     </Tooltip>
+//                   </Collapse>
+//                 </Flex>
+//                 <Flex justifyContent="space-between" alignContent="center">
+//                   <Popover isOpen={isOpen} onClose={() => setIsOpen(false)}>
+//                     <PopoverTrigger>
+//                       <div onClick={() => setIsOpen(true)}>
+//                         {/* Contenido visible del PopoverTrigger */}
+//                         <Rating ratingValue={ratingValue} handleRatingClick={handleRatingClick} />
+//                       </div>
+//                     </PopoverTrigger>
+//                     <PopoverContent>
+//                       <PopoverHeader>Rate this product</PopoverHeader>
+//                       <PopoverBody>
+//                         <Flex justifyContent="center">
+//                           <Rating ratingValue={ratingValue} handleRatingClick={handleRatingClick} />
+//                         </Flex>
+//                         <Input
+//                           placeholder="Your comment"
+//                           value={comment}
+//                           onChange={handleCommentChange}
+//                           mt={4}
+//                         />
+//                         <Button
+//                           colorScheme="blue"
+//                           mt={4}
+//                           onClick={() => setIsOpen(false)}
+//                         >
+//                           Submit
+//                         </Button>
+//                       </PopoverBody>
+//                     </PopoverContent>
+//                   </Popover>
+//                   <Box fontSize="2xl" color="gray.800">
+//                     <Box as="span" color="gray.600" fontSize="lg">
+//                       $
+//                     </Box>
+//                     {price.toFixed(2)}
+//                   </Box>
+//                 </Flex>
+//               </Box>
+//             </Flex>
+//           </Box>
+//         </VStack>
+//       </Flex>
+//     </Box>
+//   );
+// };
+
+// export default ProductAddToCart;
+
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
@@ -152,14 +349,22 @@ const ProductAddToCart = ({ id, image, name, price, rating, numReviews }) => {
                     <PopoverTrigger>
                       <div onClick={() => setIsOpen(true)}>
                         {/* Contenido visible del PopoverTrigger */}
-                        <Rating ratingValue={ratingValue} handleRatingClick={handleRatingClick} />
+                        <Rating
+                          ratingValue={ratingValue}
+                          handleRatingClick={handleRatingClick}
+                          productId={id} // Pasar el id del producto como prop
+                        />
                       </div>
                     </PopoverTrigger>
                     <PopoverContent>
                       <PopoverHeader>Rate this product</PopoverHeader>
                       <PopoverBody>
                         <Flex justifyContent="center">
-                          <Rating ratingValue={ratingValue} handleRatingClick={handleRatingClick} />
+                          <Rating
+                            ratingValue={ratingValue}
+                            handleRatingClick={handleRatingClick}
+                            productId={id} // Pasar el id del producto como prop
+                          />
                         </Flex>
                         <Input
                           placeholder="Your comment"
@@ -194,4 +399,3 @@ const ProductAddToCart = ({ id, image, name, price, rating, numReviews }) => {
 };
 
 export default ProductAddToCart;
-
--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,42 +1,124 @@
-// import React from "react";
-// import { Box, Flex } from "@chakra-ui/react";
+// import React, { useState, useEffect } from "react";
+// import { Flex, Box, Alert, AlertIcon, Popover, PopoverTrigger, PopoverContent, PopoverHeader, PopoverBody, Button, Textarea } from "@chakra-ui/react";
 // import { BsStarFill, BsStarHalf, BsStar } from "react-icons/bs";
+// import { useAuth0 } from "@auth0/auth0-react";
 
-// const Rating = ({ ratingValue, handleRatingClick }) => {
+// const Rating = ({ ratingValue, numRatings, handleRating }) => {
+//   const { isAuthenticated, loginWithRedirect, user } = useAuth0();
+//   const [hoverRating, setHoverRating] = useState(0);
+//   const [showAlert, setShowAlert] = useState(false);
+//   const [userData, setUserData] = useState({});
+//   const [reviewText, setReviewText] = useState("");
+//   const [selectedRating, setSelectedRating] = useState(0);
+
+//   useEffect(() => {
+//     if (isAuthenticated) {
+//       setUserData(user);
+//     }
+//   }, [isAuthenticated, user]);
+
 //   const renderStars = (ratingValue) => {
 //     const roundedRating = Math.round(ratingValue * 2) / 2;
-//     return Array(5)
-//       .fill("")
-//       .map((_, i) => {
-//         if (roundedRating - i >= 1) {
-//           return (
-//             <Box key={i} ml={1}>
-//               <BsStarFill
-//                 style={{ marginLeft: "1" }}
-//                 color={i < ratingValue ? "black" : "gray.300"}
-//                 onClick={() => handleRatingClick(i + 1)}
-//               />
-//             </Box>
-//           );
-//         }
-//         if (roundedRating - i === 0.5) {
-//           return (
-//             <Box key={i} ml={1}>
-//               <BsStarHalf style={{ marginLeft: "1" }} />
-//             </Box>
-//           );
-//         }
-//         return (
-//           <Box key={i} ml={1}>
-//             <BsStar style={{ marginLeft: "1" }} />
-//           </Box>
-//         );
+//     return (
+//       <Flex>
+//         {Array(5)
+//           .fill("")
+//           .map((_, i) => {
+//             let starColor = "gray.300";
+//             if (hoverRating >= i + 1) {
+//               starColor = "yellow";
+//             } else if (roundedRating >= i + 1) {
+//               starColor = "yellow";
+//             }
+//             return (
+//               <Box
+//                 key={i}
+//                 ml={1}
+//                 onMouseEnter={() => setHoverRating(i + 1)}
+//                 onMouseLeave={() => setHoverRating(0)}
+//                 onClick={isAuthenticated ? () => handleStarClick(i + 1) : handleNotAuthenticated}
+//                 style={{ cursor: isAuthenticated ? "pointer" : "not-allowed" }}
+//               >
+//                 {roundedRating - i >= 1 ? (
+//                   <BsStarFill style={{ marginLeft: "1" }} color={starColor} />
+//                 ) : roundedRating - i === 0.5 ? (
+//                   <BsStarHalf style={{ marginLeft: "1" }} color={starColor} />
+//                 ) : (
+//                   <BsStar style={{ marginLeft: "1" }} color={starColor} />
+//                 )}
+//               </Box>
+//             );
+//           })}
+//       </Flex>
+//     );
+//   };
+
+//   const handleNotAuthenticated = () => {
+//     setShowAlert(true);
+//   };
+
+//   const handleStarClick = (rating) => {
+//     setSelectedRating(rating);
+//   };
+
+//   const handleReviewChange = (e) => {
+//     setReviewText(e.target.value);
+//   };
+
+//   const handleRatingClick = () => {
+//     const data = {
+//       rate: selectedRating,
+//       review: reviewText,
+//       productId: "1008",
+//       id: 3,
+//     };
+
+//     console.log(data);
+
+//     fetch("http://localhost:3010/rating/create", {
+//       method: "POST",
+//       headers: {
+//         "Content-Type": "application/json",
+//         Authorization: `Bearer ${userData.token}`,
+//       },
+//       body: JSON.stringify(data),
+//     })
+//       .then((response) => response.json())
+//       .then((result) => {
+//         console.log(result);
+//       })
+//       .catch((error) => {
+//         console.error(error);
 //       });
 //   };
 
 //   return (
 //     <Flex alignItems="center">
-//       {renderStars(ratingValue)}
+//       <Popover>
+//         <PopoverTrigger>
+//           <div>{renderStars(ratingValue)}</div>
+//         </PopoverTrigger>
+//         <PopoverContent>
+//           <PopoverHeader>Rate this product</PopoverHeader>
+//           <PopoverBody>
+//             <Textarea
+//               value={reviewText}
+//               onChange={handleReviewChange}
+//               placeholder="Write a review..."
+//               mb={3}
+//             />
+//             <Button colorScheme="blue" onClick={handleRatingClick}>
+//               Submit Rating
+//             </Button>
+//           </PopoverBody>
+//         </PopoverContent>
+//       </Popover>
+//       {showAlert && (
+//         <Alert status="warning" ml={2}>
+//           <AlertIcon />
+//           Please log in to rate this product.
+//         </Alert>
+//       )}
 //     </Flex>
 //   );
 // };
@@ -44,50 +126,139 @@
 // export default Rating;
 
 
-import React, { useState } from "react";
-import { Box, Flex } from "@chakra-ui/react";
+import React, { useState, useEffect } from "react";
+import {
+  Flex,
+  Box,
+  Alert,
+  AlertIcon,
+  Popover,
+  PopoverTrigger,
+  PopoverContent,
+  PopoverHeader,
+  PopoverBody,
+  Button,
+  Textarea,
+} from "@chakra-ui/react";
 import { BsStarFill, BsStarHalf, BsStar } from "react-icons/bs";
+import { useAuth0 } from "@auth0/auth0-react";
 
-const Rating = ({ ratingValue, numRatings, handleRatingClick }) => {
+const Rating = ({ ratingValue, numRatings, handleRating, productId }) => {
+  const { isAuthenticated, loginWithRedirect, user } = useAuth0();
   const [hoverRating, setHoverRating] = useState(0);
+  const [showAlert, setShowAlert] = useState(false);
+  const [userData, setUserData] = useState({});
+  const [reviewText, setReviewText] = useState("");
+  const [selectedRating, setSelectedRating] = useState(0);
+
+  useEffect(() => {
+    if (isAuthenticated) {
+      setUserData(user);
+    }
+  }, [isAuthenticated, user]);
 
   const renderStars = (ratingValue) => {
     const roundedRating = Math.round(ratingValue * 2) / 2;
-    return Array(5)
-      .fill("")
-      .map((_, i) => {
-        let starColor = "gray.300";
-        if (hoverRating >= i + 1) {
-          starColor = "yellow";
-        } else if (roundedRating >= i + 1) {
-          starColor = "yellow";
-        }
-        return (
-          <Box
-            key={i}
-            ml={1}
-            onMouseEnter={() => setHoverRating(i + 1)}
-            onMouseLeave={() => setHoverRating(0)}
-            onClick={() => handleRatingClick(i + 1)}
-          >
-            {roundedRating - i >= 1 ? (
-              <BsStarFill style={{ marginLeft: "1" }} color={starColor} />
-            ) : roundedRating - i === 0.5 ? (
-              <BsStarHalf style={{ marginLeft: "1" }} color={starColor} />
-            ) : (
-              <BsStar style={{ marginLeft: "1" }} color={starColor} />
-            )}
-          </Box>
-        );
+    return (
+      <Flex>
+        {Array(5)
+          .fill("")
+          .map((_, i) => {
+            let starColor = "gray.300";
+            if (hoverRating >= i + 1) {
+              starColor = "yellow";
+            } else if (roundedRating >= i + 1) {
+              starColor = "yellow";
+            }
+            return (
+              <Box
+                key={i}
+                ml={1}
+                onMouseEnter={() => setHoverRating(i + 1)}
+                onMouseLeave={() => setHoverRating(0)}
+                onClick={isAuthenticated ? () => handleStarClick(i + 1) : handleNotAuthenticated}
+                style={{ cursor: isAuthenticated ? "pointer" : "not-allowed" }}
+              >
+                {roundedRating - i >= 1 ? (
+                  <BsStarFill style={{ marginLeft: "1" }} color={starColor} />
+                ) : roundedRating - i === 0.5 ? (
+                  <BsStarHalf style={{ marginLeft: "1" }} color={starColor} />
+                ) : (
+                  <BsStar style={{ marginLeft: "1" }} color={starColor} />
+                )}
+              </Box>
+            );
+          })}
+      </Flex>
+    );
+  };
+
+  const handleNotAuthenticated = () => {
+    setShowAlert(true);
+  };
+
+  const handleStarClick = (rating) => {
+    setSelectedRating(rating);
+  };
+
+  const handleReviewChange = (e) => {
+    setReviewText(e.target.value);
+  };
+
+  const handleRatingClick = () => {
+    const data = {
+      rate: selectedRating,
+      review: reviewText,
+      productId: productId,
+      id: 5,
+    };
+
+    console.log(data);
+
+    fetch("http://localhost:3010/rating/create", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${userData.token}`,
+      },
+      body: JSON.stringify(data),
+    })
+      .then((response) => response.json())
+      .then((result) => {
+        console.log(result);
+      })
+      .catch((error) => {
+        console.error(error);
       });
   };
 
   return (
     <Flex alignItems="center">
-      {renderStars(ratingValue)}
-      {/* <Text ml={2} fontSize="sm" color="gray.500">
-        ({numRatings} Ratings)
-      </Text> */}
+      <Popover>
+        <PopoverTrigger>
+          <div>{renderStars(ratingValue)}</div>
+        </PopoverTrigger>
+        <PopoverContent>
+          <PopoverHeader>Rate this product</PopoverHeader>
+          <PopoverBody>
+            <Textarea
+              value={reviewText}
+              onChange={handleReviewChange}
+              placeholder="Write a review..."
+              mb={3}
+            />
+            <Button colorScheme="blue" onClick={handleRatingClick}>
+              Submit Rating
+            </Button>
+          </PopoverBody>
+        </PopoverContent>
+      </Popover>
+      {showAlert && (
+        <Alert status="warning" ml={2}>
+          <AlertIcon />
+          Please log in to rate this product.
+        </Alert>
+      )}
     </Flex>
   );
 };
