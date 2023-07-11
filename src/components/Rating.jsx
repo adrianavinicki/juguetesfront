94cde441d331224d4cac26973877371eaabfd67e
¨kervys¨ <kervysjulian@gmail.com>
2023-07-10 20:16:04 -0400

    Cambios minimos

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import {
   Flex,
   Box,
@@ -11,21 +11,12 @@ import {
   Textarea,
 } from "@chakra-ui/react";
 import { BsStarFill, BsStarHalf, BsStar } from "react-icons/bs";
-import { useAuth0 } from "@auth0/auth0-react";
 
-const Rating = ({ ratingValue, numRatings, handleRating, productId }) => {
-  const { isAuthenticated, user } = useAuth0();
+const Rating = ({ ratingValue, numRatings, handleRating, productId, userId }) => {
   const [hoverRating, setHoverRating] = useState(0);
-  const [userData, setUserData] = useState({});
-  const [selectedRating, setSelectedRating] = useState(0);
   const [reviewText, setReviewText] = useState("");
-  const [isOpen, setIsOpen] = useState(false);
-
-  useEffect(() => {
-    if (isAuthenticated) {
-      setUserData(user);
-    }
-  }, [isAuthenticated, user]);
+  const [selectedRating, setSelectedRating] = useState(0);
+  const [isRatingSubmitted, setIsRatingSubmitted] = useState(false);
 
   const renderStars = (ratingValue) => {
     const roundedRating = Math.round(ratingValue * 2) / 2;
@@ -46,8 +37,8 @@ const Rating = ({ ratingValue, numRatings, handleRating, productId }) => {
                 ml={1}
                 onMouseEnter={() => setHoverRating(i + 1)}
                 onMouseLeave={() => setHoverRating(0)}
-                onClick={isAuthenticated ? () => handleStarClick(i + 1) : undefined}
-                style={{ cursor: isAuthenticated ? "pointer" : "not-allowed" }}
+                onClick={() => handleStarClick(i + 1)}
+                style={{ cursor: "pointer" }}
               >
                 {roundedRating - i >= 1 ? (
                   <BsStarFill style={{ marginLeft: "1" }} color={starColor} />
@@ -65,19 +56,20 @@ const Rating = ({ ratingValue, numRatings, handleRating, productId }) => {
 
   const handleStarClick = (rating) => {
     setSelectedRating(rating);
-    setIsOpen(false);
   };
 
   const handleReviewChange = (e) => {
     setReviewText(e.target.value);
   };
+  console.log("desde rating " + userId)
 
   const handleRatingClick = () => {
     const data = {
       rate: selectedRating,
       review: reviewText,
       productId: productId,
-      id: 5,
+      // id: userId, 
+      id: userId// Usar el ID del usuario proporcionado
     };
 
     console.log(data);
@@ -86,13 +78,16 @@ const Rating = ({ ratingValue, numRatings, handleRating, productId }) => {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
-        Authorization: `Bearer ${userData.token}`,
       },
       body: JSON.stringify(data),
     })
       .then((response) => response.json())
       .then((result) => {
         console.log(result);
+        // Limpiar el formulario
+        setSelectedRating(0);
+        setReviewText("");
+        setIsRatingSubmitted(true);
       })
       .catch((error) => {
         console.error(error);
@@ -101,28 +96,28 @@ const Rating = ({ ratingValue, numRatings, handleRating, productId }) => {
 
   return (
     <Flex alignItems="center">
-      <Popover isOpen={isOpen} onClose={() => setIsOpen(false)}>
+      <Popover>
         <PopoverTrigger>
-          <div
-            onMouseEnter={() => setIsOpen(true)}
-            onMouseLeave={() => setIsOpen(false)}
-            style={{ cursor: "pointer" }}
-          >
-            {renderStars(selectedRating || ratingValue)}
-          </div>
+          <div>{renderStars(ratingValue)}</div>
         </PopoverTrigger>
         <PopoverContent>
           <PopoverHeader>Rate this product</PopoverHeader>
           <PopoverBody>
-            <Textarea
-              value={reviewText}
-              onChange={handleReviewChange}
-              placeholder="Write a review..."
-              mb={3}
-            />
-            <Button colorScheme="blue" onClick={handleRatingClick}>
-              Submit Rating
-            </Button>
+            {!isRatingSubmitted ? (
+              <>
+                <Textarea
+                  value={reviewText}
+                  onChange={handleReviewChange}
+                  placeholder="Write a review..."
+                  mb={3}
+                />
+                <Button colorScheme="blue" onClick={handleRatingClick}>
+                  Submit Rating
+                </Button>
+              </>
+            ) : (
+              <Box>Thank you for your rating!</Box>
+            )}
           </PopoverBody>
         </PopoverContent>
       </Popover>
@@ -131,94 +126,3 @@ const Rating = ({ ratingValue, numRatings, handleRating, productId }) => {
 };
 
 export default Rating;
-
-
-// import React, { useState } from "react";
-// import { Flex, Box, Button, Textarea } from "@chakra-ui/react";
-// import { useAuth0 } from "@auth0/auth0-react";
-
-// const Rating = ({ ratingValue, numRatings, handleRating, productId }) => {
-//   const { isAuthenticated, user } = useAuth0();
-//   const [hoverRating, setHoverRating] = useState(0);
-//   const [userData, setUserData] = useState({});
-//   const [selectedRating, setSelectedRating] = useState(ratingValue);
-//   const [reviewText, setReviewText] = useState("");
-//   const [showRatingForm, setShowRatingForm] = useState(false);
-
-//   const handleStarClick = (rating) => {
-//     if (isAuthenticated) {
-//       setSelectedRating(rating);
-//       setShowRatingForm(true);
-//     }
-//   };
-
-//   const handleReviewChange = (e) => {
-//     setReviewText(e.target.value);
-//   };
-
-//   const handleRatingClick = () => {
-//     const data = {
-//       rate: selectedRating,
-//       review: reviewText,
-//       productId: productId,
-//       id: 5,
-//     };
-
-//     console.log(data);
-
-//     // Aquí puedes realizar la llamada al backend para enviar la calificación
-
-//     setShowRatingForm(false);
-//   };
-
-//   return (
-//     <Flex alignItems="center">
-//       <Flex>
-//         {Array(5)
-//           .fill("")
-//           .map((_, i) => {
-//             let starColor = "gray.300";
-//             if (hoverRating >= i + 1) {
-//               starColor = "yellow";
-//             } else if (selectedRating >= i + 1) {
-//               starColor = "yellow";
-//             }
-//             return (
-//               <Box
-//                 key={i}
-//                 ml={1}
-//                 onMouseEnter={() => setHoverRating(i + 1)}
-//                 onMouseLeave={() => setHoverRating(0)}
-//                 onClick={() => handleStarClick(i + 1)}
-//                 style={{ cursor: isAuthenticated ? "pointer" : "not-allowed" }}
-//               >
-//                 <span role="img" aria-label="star" style={{ marginRight: "0.2em" }}>
-//                   {selectedRating >= i + 1 ? "⭐️" : "☆"}
-//                 </span>
-//               </Box>
-//             );
-//           })}
-//       </Flex>
-
-//       {showRatingForm && (
-//         <div>
-//           <p>Rating: {selectedRating}</p>
-//           <Textarea
-//             value={reviewText}
-//             onChange={handleReviewChange}
-//             placeholder="Write a review..."
-//             mb={3}
-//           />
-//           <Button colorScheme="blue" onClick={handleRatingClick}>
-//             Submit Rating
-//           </Button>
-//           <Button variant="ghost" onClick={() => setShowRatingForm(false)}>
-//             Cancel
-//           </Button>
-//         </div>
-//       )}
-//     </Flex>
-//   );
-// };
-
-// export default Rating;
--- src/index.jsx
+++ src/index.jsx
@@ -1,3 +1,37 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import { ChakraProvider } from '@chakra-ui/react';
+// import { Provider } from 'react-redux';
+// import { Auth0Provider } from '@auth0/auth0-react';
+// import { persistStore } from 'redux-persist';
+// import { PersistGate } from 'redux-persist/integration/react';
+// import App from './App';
+// import store from './redux/store';
+// import { LoginAuth } from './views/Login/LoginAuth';
+// import { Rating } from './components/Rating';
+
+// const persistor = persistStore(store);
+
+// ReactDOM.render(
+//   <Auth0Provider
+//     domain="wondertoyshenry.us.auth0.com"
+//     clientId="pmqIpRA46YXGq9RBHCY0BG7calWTBYWG"
+//     redirectUri={window.location.origin}
+//     cacheLocation="localstorage" // Guarda el estado de la sesión en el almacenamiento local
+//   >
+//     <ChakraProvider>
+//       <Provider store={store}>
+//         <PersistGate persistor={persistor}>
+//           <App />
+//           <LoginAuth />
+//           <Rating />
+//         </PersistGate>
+//       </Provider>
+//     </ChakraProvider>
+//   </Auth0Provider>,
+//   document.getElementById('root')
+// );
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { ChakraProvider } from '@chakra-ui/react';
@@ -8,6 +42,7 @@ import { PersistGate } from 'redux-persist/integration/react';
 import App from './App';
 import store from './redux/store';
 import { LoginAuth } from './views/Login/LoginAuth';
+import Rating from './components/Rating';
 
 const persistor = persistStore(store);
 
@@ -21,9 +56,9 @@ ReactDOM.render(
     <ChakraProvider>
       <Provider store={store}>
         <PersistGate persistor={persistor}>
-          
           <App />
           <LoginAuth />
+          <Rating />
         </PersistGate>
       </Provider>
     </ChakraProvider>
